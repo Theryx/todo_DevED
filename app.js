3d023bc50ba306640c6f9b6ec20d446328b31ad1
Africa <ndouken@gmail.com>
2021-01-12 01:49:10 +0100

    Todo completed

--- app.js
+++ app.js
@@ -2,11 +2,84 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 //Event Listener
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 //function
 function addTodo(event) {
   //Prevent form from submitting
   event.preventDefault();
-  console.log("Hello");
+  //Todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create Li
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
+  newTodo.innerText = todoInput.value;
+  todoDiv.appendChild(newTodo);
+  //CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>`;
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+  //CHECK TRASH BUTTON
+  const trash = document.createElement("button");
+  trash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>`;
+  trash.classList.add("trash-btn");
+  todoDiv.appendChild(trash);
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+  //Clear todo input value
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  //Delete TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //Check Mark
+
+  if (item.classList[0] === "completed-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
